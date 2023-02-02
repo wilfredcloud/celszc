4237c235a3be0809c7c4db89e698023a7f498be2
Wilfred <wilfredcloudspace@gmail.com>
2023-02-02 11:04:00 +0100

    Create some basics components
    These components include Hero, Marquee, Navbar, SearchBanner

---
+++ src/components/SearchBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchBanner = () => {
+  return (
+    <div>SearchBanner</div>
+  )
+}
+
+export default SearchBanner
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Marquee} from './Marquee';
+export {default as Navbar} from './Navbar';
+// export {default as Marquee} from './Marquee';
+// export {default as Marquee} from './Marquee';
+// export {default as Marquee} from './Marquee';

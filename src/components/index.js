d84e247f23d905f4ea6c575c7234d1bffdfb7181
Wilfred <wilfredcloudspace@gmail.com>
2023-02-03 18:28:38 +0100

    Update export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export {default as Marquee} from './Marquee';
 export {default as Navbar} from './Navbar';
-// export {default as Marquee} from './Marquee';
-// export {default as Marquee} from './Marquee';
+export {default as ProfileDropdown} from './ProfileDropdown';
+export {default as Hero} from './Hero';
 // export {default as Marquee} from './Marquee';

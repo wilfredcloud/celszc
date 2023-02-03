f7c1b551b1afc2839e73f0cfed174dd3fbacaefc
Wilfred <wilfredcloudspace@gmail.com>
2023-02-03 16:32:23 +0100

    Build Marquee component

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Marquee = () => {
   return (
-    <div>Marquee</div>
+    <div className=" bg-blue-400 text-gray-900 text-center text-sm p-1">
+      <b>PRAY WITH PASTOR CHRIST</b>
+        <span className=" ml-3 ">
+          Join Pastor Christ every Monday, Wednesday, and Friday by 10am and
+          12pm (GMT +1) as he prays for 15 Minutes
+        </span>
+    </div>
   )
 }
 

2f28e188bd4f840472ee826ebbfcc21a91701ba2
Wilfred <wilfredcloudspace@gmail.com>
2023-02-03 18:29:47 +0100

    Add background image to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import HeroBanner from '../assets/hero.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div >
+      <div style={{backgroundImage: `url(${HeroBanner})`}}
+      className=" h-[546px]">
+
+      </div>
+     
+    </div>
   )
 }
 

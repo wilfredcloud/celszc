f7ee8f49cf1cb5d2475725d473ffc32d16ba0bf6
Wilfred <wilfredcloudspace@gmail.com>
2023-02-04 10:19:28 +0100

    Correct Christ to Chris

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Marquee = () => {
   return (
     <div className=" bg-blue-400 text-gray-900 text-center text-sm p-1">
-      <b>PRAY WITH PASTOR CHRIST</b>
+      <b>PRAY WITH PASTOR CHRIS</b>
         <marquee className=" ml-3 ">
-          Join Pastor Christ every Monday, Wednesday, and Friday by 10am and
+          Join Pastor Chris every Monday, Wednesday, and Friday by 10am and
           12pm (GMT +1) as he prays for 15 Minutes
         </marquee>
     </div>

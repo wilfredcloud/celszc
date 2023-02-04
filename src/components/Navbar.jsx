41cbb6c822a6719955b3d2cff26a35216a7e2f80
Wilfred <wilfredcloudspace@gmail.com>
2023-02-04 10:14:01 +0100

    Update navbar to make it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,9 +28,12 @@ const Navbar = () => {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center
+                 rounded-md p-2 text-gray-400 hover:bg-gray-700
+                  hover:text-white focus:outline-none focus:ring-2 
+                  focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -42,17 +45,17 @@ const Navbar = () => {
               {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="block h-10 w-auto ml-20 lg:hidden "
+                    className="block h-10 w-auto ml-16 md:hidden "
                     src={Logo}
                     alt="CE Lagos Sub Zone C"
                   />
                   <img
-                    className="hidden h-10 w-auto lg:block"
+                    className="hidden h-10 w-auto md:block"
                     src={Logo}
                     alt="CE Lagos Sub Zone C"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden sm:ml-6 md:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
@@ -87,7 +90,7 @@ const Navbar = () => {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
                 <Disclosure.Button

b582ed760cc08a2ac44f5cfa6f24da49914c7f8c
Wilfred <wilfredcloudspace@gmail.com>
2023-02-06 10:37:30 +0100

    Add row component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export {default as Marquee} from './Marquee';
 export {default as Navbar} from './Navbar';
 export {default as ProfileDropdown} from './ProfileDropdown';
 export {default as Hero} from './Hero';
-// export {default as Marquee} from './Marquee';
+export {default as Row} from './Row';

de4f8ab23653630b7b272964e9e7855260803df8
Wilfred <wilfredcloudspace@gmail.com>
2023-02-08 12:31:44 +0100

    Format JumboRow component to make it more readable

--- src/components/JumboRow.jsx
+++ src/components/JumboRow.jsx
@@ -4,10 +4,15 @@ const JumboRow = ({data, handleClick}) => (
     <div className=" container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 my-20 px-4 ">
         <div className={`${data?.order == 'reserved' ? 'lg:order-2' : ''}`}>
             {data?.hr && <hr className={`w-32 md:w-40 mb-6 border-2 md:border-4 ${data?.hr?.class}`}/>}
-            <h1 style={{ fontFamily: data?.title?.fontFamily }} className={`text-3xl md:text-4xl font-[900] ${data?.title.class}`}>{data.title.content}</h1>
+            <h1 style={{ fontFamily: data?.title?.fontFamily }} 
+            className={`text-3xl md:text-4xl font-[900] ${data?.title.class}`}>
+                {data.title.content}</h1>
             <p className=" text-lg md:text-xl mt-4 leading-8 md:leading-10">{data.text}</p>
-           {data?.button &&  <button onClick={handleClick} className={`rounded-md  p-2.5 md:p-3.5 text-base font-semibold leading-7 text-white shadow-sm 
-        focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  uppercase mt-3 ${data?.button?.class}`}>{data?.button?.text}</button>}
+           {data?.button &&  <button onClick={handleClick} 
+           className={`rounded-md  p-2.5 md:p-3.5 text-base font-semibold leading-7
+            text-white shadow-sm focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2  uppercase mt-3 ${data?.button?.class}`}>
+                {data?.button?.text}</button>}
         </div>
         <div className={`${data?.order == 'reserved'? 'lg:order-1' : ''}`}>
             <img src={data.image} alt="Worshipper" />

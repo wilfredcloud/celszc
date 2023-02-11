557ffb27ca0ff5acb5f2226b4f92b447a362e348
Wilfred <wilfredcloudspace@gmail.com>
2023-02-11 23:29:56 +0100

    Add reactplayer to video component to play videos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ReactPlayer from "react-player"
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className=" mx-4 bg-slate-900 flex justify-center">
+      <ReactPlayer url="https://www.youtube.com/watch?v=ge-PI-eQcSs" className=" w-full react-player" controls/>
+    </div>
   )
 }
 

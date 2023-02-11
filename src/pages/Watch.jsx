ef74b59e6070a458086e9233da43b240087d7465
Wilfred <wilfredcloudspace@gmail.com>
2023-02-11 23:32:17 +0100

    Update watch page

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -4,7 +4,7 @@ import { Comment, Video } from "../components"
 const Watch = () => {
   return (
     <div>
-        <div className="container flex flex-col lg:flex-row justify-start">
+        <div className="container mx-auto flex flex-col lg:flex-row justify-start">
            <div className="w-full lg:w-4/6">
            <Video/>
            </div>
@@ -12,7 +12,6 @@ const Watch = () => {
            <div className="w-full lg:w-2/6">
             <Comment/>
            </div>
-            <Comment/>
         </div>
     </div>
   )

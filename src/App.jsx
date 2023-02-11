31f58c6d7ddfb7b1465fb25781721271cd98f6b8
Wilfred <wilfredcloudspace@gmail.com>
2023-02-11 05:21:15 +0100

    Add footer to App component so it can be outside the route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { Home, Login, Watch } from './pages'
-import { Marquee, Navbar } from './components'
+import { Footer, Marquee, Navbar } from './components'
 
 const App = () => (
   <div>
@@ -15,6 +15,7 @@ const App = () => (
         <Route path='/watch' element={<Watch/>} />
       </Routes>
     </BrowserRouter>
+    <Footer/>
   </div>
 )
 

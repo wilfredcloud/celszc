503f263760008fd484d617999a91cb567f6f6e0f
Wilfred <wilfredcloudspace@gmail.com>
2023-02-11 05:42:01 +0100

    Add video and comment component to watch page

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Comment, Video } from "../components"
 
 const Watch = () => {
   return (
-    <div>Watch</div>
+    <div>
+        <div className="container flex flex-col lg:flex-row justify-start">
+           <div className="w-full lg:w-4/6">
+           <Video/>
+           </div>
+
+           <div className="w-full lg:w-2/6">
+            <Comment/>
+           </div>
+            <Comment/>
+        </div>
+    </div>
   )
 }
 

3dcbcde65d89b1db17c433d7baa0fd3b2a5ea093
Wilfred <wilfredcloudspace@gmail.com>
2023-02-13 18:09:06 +0100

    Add title to video

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -3,10 +3,14 @@ import { Comment, Video } from "../components"
 
 const Watch = () => {
   return (
-    <div>
+    <div className=" bg-slate-100 py-2">
         <div className="container mx-auto flex flex-col lg:flex-row justify-start">
            <div className="w-full lg:w-4/6">
            <Video/>
+
+           <div className="px-4 py-2">
+              <h1 className=" text-2xl font-medium">First Service Sunday Service</h1>
+           </div>
            </div>
 
            <div className="w-full lg:w-2/6">

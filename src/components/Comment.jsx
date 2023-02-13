549896ea398001655df71a803c584b545f6177b9
Wilfred <wilfredcloudspace@gmail.com>
2023-02-13 19:41:06 +0100

    Remove background color from comment body

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,7 +5,7 @@ const Comment = () => {
     <div className="border comment-section flex flex-col justify-between">
       <div className=" bg-white w-full h-12 px-4 flex flex-col justify-center text-lg
       font-medium text-gray-700">Comments</div>
-      <div className=" bg-slate-400 flex-1"> chat body</div>
+      <div className=" flex-1"> chat body</div>
       <div className=" bg-white h-12">
           inputs
       </div>

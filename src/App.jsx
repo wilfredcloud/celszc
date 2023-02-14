c33f7a2784c50cd65153300ae61dd412ed0d16a8
Wilfred <wilfredcloudspace@gmail.com>
2023-02-14 12:08:12 +0100

    Add login modal component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { Home, Login, Watch } from './pages'
-import { Footer, Marquee, Navbar } from './components'
+import { Footer, LoginModal, Marquee, Navbar } from './components'
 
 const App = () => (
   <div>
@@ -15,6 +15,7 @@ const App = () => (
         <Route path='/watch' element={<Watch/>} />
       </Routes>
     </BrowserRouter>
+    <LoginModal/>
     <Footer/>
   </div>
 )

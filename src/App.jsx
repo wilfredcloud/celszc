5d8fb3cad919b6822090af94be4ebb7e6d1e8613
Wilfred <wilyfrez@gmail.com>
2023-02-20 11:19:05 +0100

    Wrap app with AppContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Box } from '@mui/material'
-import { Home, Login, Watch } from './pages'
-import { Footer, LoginModal, Marquee, Navbar } from './components'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppContext } from './context/AppContext';
+import { Home, Login, Watch } from './pages';
+import { Footer, LoginModal, Marquee, Navbar } from './components';
 
-const App = () => (
-  <div>
-    <Marquee/>
-    <Navbar/>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/watch' element={<Watch/>} />
-      </Routes>
-    </BrowserRouter>
-    <LoginModal/>
-    <Footer/>
-  </div>
-)
+const App = () => {
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
-export default App
+  return (
+    <AppContext.Provider value={{ showLoginModal, setShowLoginModal }}>
+      <Marquee />
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/watch" element={<Watch />} />
+        </Routes>
+      </BrowserRouter>
+      <LoginModal />
+      <Footer />
+    </AppContext.Provider>
+  );
+};
+
+export default App;

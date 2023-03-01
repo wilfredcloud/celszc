e371f4212ae6b4766129cf84843dfd762ead46df
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-01 20:04:43 +0100

    Remove comments

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -48,10 +48,8 @@ const Login = ({ setAuthView }) => {
     console.log(email);
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
         const user = userCredential.user;
         console.log(user);
-        // ...
       })
       .catch((error) => {
         const errorCode = error.code;

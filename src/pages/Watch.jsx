722c61dbb9083cebb28ce9d92f9b4f77019dee73
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-03 23:34:08 +0100

    Make bgcolor white

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Comment, Video } from "../components"
+import React from 'react';
+import { Comment, Video } from '../components';
 
 const Watch = () => {
   return (
-    <div className=" bg-slate-100 py-2">
-        <div className="container mx-auto flex flex-col lg:flex-row justify-start">
-           <div className="w-full lg:w-4/6">
-           <Video/>
+    <div className=" py-2">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-start">
+        <div className="w-full lg:w-4/6">
+          <Video />
 
-           <div className="px-4 py-2">
-              <h1 className=" text-2xl font-medium">First Service Sunday Service</h1>
-           </div>
-           </div>
-
-           <div className="w-full lg:w-2/6">
-            <Comment/>
-           </div>
+          <div className="px-4 py-2">
+            <h1 className=" text-2xl font-medium">
+              First Service Sunday Service
+            </h1>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Watch
+        <div className="w-full lg:w-2/6">
+          <Comment />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Watch;

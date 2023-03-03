7db565b8d9fcdb673a4c0c12e9250cb45463a5ae
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-03 23:33:39 +0100

    Add tailwind classes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BiMessageSquareDots } from 'react-icons/bi';
 
 const Comment = () => {
   return (
-    <div className="border comment-section flex flex-col justify-between">
-      <div className=" bg-white w-full h-12 px-4 flex flex-col justify-center text-lg
-      font-medium text-gray-700">Comments</div>
-      <div className=" flex-1"> chat body</div>
-      <div className=" bg-white h-12">
-          inputs
+    <div className="border rounded-xl comment-section flex flex-col justify-between">
+      <div
+        className=" bg-white w-full h-12 px-4 flex flex-row items-center justify-start space-x-2 text-lg
+      font-medium text-gray-700 border-b"
+      >
+        <BiMessageSquareDots size={30} /> <span>Live Chats</span>
       </div>
+      <div className=" flex-1 px-2"> chat body</div>
+      <div className=" bg-white h-12">inputs</div>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;

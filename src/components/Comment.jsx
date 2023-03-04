2584408d4e8a62b2195d46547feaf0518cba4fcd
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-04 04:34:20 +0100

    Add chat input

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BiMessageSquareDots } from 'react-icons/bi';
-
+import { RiSendPlane2Fill } from 'react-icons/ri';
 const Comment = () => {
   return (
     <div className="border rounded-xl comment-section flex flex-col justify-between">
@@ -10,8 +10,17 @@ const Comment = () => {
       >
         <BiMessageSquareDots size={30} /> <span>Live Chats</span>
       </div>
-      <div className=" flex-1 px-2"> chat body</div>
-      <div className=" bg-white h-12">inputs</div>
+      <div className=" flex-1 px-2"></div>
+      <div className=" bg-white  flex flex-row justify-between items-center border-t">
+        <input
+          type="text"
+          className="border-0 flex-1 focus:outline-none placeholder:italic px-2 py-3"
+          placeholder="Chat here"
+        />
+        <button className=" text-blue-800 pr-1">
+          {<RiSendPlane2Fill size={32} />}
+        </button>
+      </div>
     </div>
   );
 };

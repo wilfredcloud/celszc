1e75034636f0bf772b6c5bebdd71a112df9cba4a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-04 09:50:21 +0100

    Add ChatMessage component

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Avatar } from 'antd';
+
+const ChatMessage = ({ user, message }) => {
+  return (
+    <div>
+      <span>I love you</span>
+    </div>
+  );
+};
+
+export default ChatMessage;

0ce90856afbdee2e6870598b3b6e53c55cedd52b
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-04 10:13:00 +0100

    Update ChatMessage

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -4,7 +4,13 @@ import { Avatar } from 'antd';
 const ChatMessage = ({ user, message }) => {
   return (
     <div>
-      <span>I love you</span>
+      <div className=" bg-slate-100 p-2 rounded-lg rounded-bl-none mb-2">
+        <p className=" font-semibold">Wilfred</p>
+        <p className=" text-md">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          inventore.
+        </p>
+      </div>
     </div>
   );
 };
